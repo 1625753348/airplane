0427f6681ac1184fceabdbc558ac35f01386c63d
1625753348 <56570861+1625753348@users.noreply.github.com>
2019-12-29 15:18:43 +0800

    实现了子弹打到奖励机会累计的 分从而的到子弹加强

--- aierplane/src/com/pencil/airplane/Award.java
+++
@@ -1,9 +0,0 @@
-package com.pencil.airplane;
-
-public interface Award {
-	//定义奖励常量
-		int LIFE=0;
-		int DOUBLE_FIRE=1;
-	//定义获得的奖励
-		public int awardType();
-}
--- aierplane/src/com/pencil/airplane/Score.java
+++
@@ -1,6 +0,0 @@
-package com.pencil.airplane;
-
-public interface Score {
-	//返回一个分数
-	public int getScore();
-}
